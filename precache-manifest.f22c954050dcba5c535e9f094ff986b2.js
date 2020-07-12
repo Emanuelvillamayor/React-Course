1f2d46325d38768c12fc1e8be097a1758610809f
agustinezequielgomez <agusgomez99@gmail.com>
2020-07-12 20:58:37 -0300

    Updates

---
+++ precache-manifest.f22c954050dcba5c535e9f094ff986b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdcf3d84aa8af5608df05c8bbd619fc",
+    "url": "/React-Course/index.html"
+  },
+  {
+    "revision": "4a390bdf97a8b5c2b16a",
+    "url": "/React-Course/static/css/main.a8db2b04.chunk.css"
+  },
+  {
+    "revision": "724ff5f7fbcfae15cd6c",
+    "url": "/React-Course/static/js/2.1df26df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Course/static/js/2.1df26df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a390bdf97a8b5c2b16a",
+    "url": "/React-Course/static/js/main.68545626.chunk.js"
+  },
+  {
+    "revision": "f7d6c42e203823bc38d8",
+    "url": "/React-Course/static/js/runtime-main.4baf1f0b.js"
+  }
+]);
